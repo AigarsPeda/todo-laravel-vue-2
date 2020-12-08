3eb63e2638b49db23f62494b3921282490212ebd
Aigars Peda <aigarspeda@gmail.com>
2020-12-08 11:02:39 +0200

    Create TodoList.vue

---
+++ src/components/TodoList.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="todo-list">
+    <input
+      type="text"
+      class="todo-input"
+      placeholder="What needs to be done?"
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TodoList"
+};
+</script>
+
+<style lang="scss" scoped>
+.todo-list {
+  .todo-input {
+    width: 100%;
+    padding: 10px 18px;
+    font-size: 18px;
+    margin-bottom: 16px;
+
+    &:focus {
+      outline: 0;
+    }
+  }
+}
+</style>
