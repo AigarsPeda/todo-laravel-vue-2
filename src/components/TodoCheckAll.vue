87c028a453c68c3486bdc17263c383cc73f82cdf
Aigars Peda <aigarspeda@gmail.com>
2020-12-10 08:41:54 +0200

    Create TodoCheckAll.vue

---
+++ src/components/TodoCheckAll.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <label>
+      <input type="checkbox" :checked="!anyRemaining" @change="allChecked" />
+      Check All
+    </label>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TodoCheckAll",
+  props: {
+    anyRemaining: {
+      type: Boolean,
+      required: true
+    }
+  },
+  methods: {
+    allChecked() {
+      this.$emit("checkAllChanged", this.anyRemaining);
+    }
+  }
+};
+</script>
