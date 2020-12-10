c010d4aeb2590a25b4b6022063e98a7a1d2a67a6
Aigars Peda <aigarspeda@gmail.com>
2020-12-10 08:42:14 +0200

    Create TodoClearCompleted.vue

---
+++ src/components/TodoClearCompleted.vue
@@ -0,0 +1,22 @@
+<template>
+  <button v-if="showClearCompletedButton" @click="clearCompleted">
+    Clear Completed
+  </button>
+</template>
+
+<script>
+export default {
+  name: "TodoClearCompleted",
+  props: {
+    showClearCompletedButton: {
+      type: Boolean,
+      required: true
+    }
+  },
+  methods: {
+    clearCompleted() {
+      this.$emit("clearCompletedTodos");
+    }
+  }
+};
+</script>
